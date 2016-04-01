47cac7570d0f1d555cfa535932d1314c087d7d87
jgkaplan <joshuagraykaplan@gmail.com>
2016-04-01 00:59:07 -0500

    Updated Download Page
    Updated Download Page and removed potentially unused css styles

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,9 @@
 $(document).on('ready',function(){
     $('#copyright-year').text(new Date().getFullYear());
+	$('body').on('click','.see-more-link',function(e){
+		e.preventDefault();
+		$(this).blur();
+		$('#'+$(this).attr('data-more')).slideToggle('fast','linear');
+		$(this).text($(this).text()=='Show More'?'Show Less':'Show More');
+	});
 });
