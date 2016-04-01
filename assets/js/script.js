d176a02251e876afa840e83c1f70cd027d50dde7
jgkaplan <joshuagraykaplan@gmail.com>
2016-04-01 14:30:18 -0500

    Expansion on Download Page

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,13 @@ $(document).on('ready',function(){
 		e.preventDefault();
 		$(this).blur();
 		$('#'+$(this).attr('data-more')).slideToggle('fast','linear');
-		$(this).text($(this).text()=='Show More'?'Show Less':'Show More');
+		$('.see-more-text',this).text($('.see-more-text', this).text()=='Show More'?'Show Less':'Show More');
+		if($('.see-more-text', this).text()=='Show Less'){
+			$('.download-arrow', this).css('border-bottom', '5px solid currentColor');
+			$('.download-arrow', this).css('border-top', 'none');
+		}else{
+			$('.download-arrow', this).css('border-top', '5px solid currentColor');
+			$('.download-arrow', this).css('border-bottom', 'none');
+		}
 	});
 });
